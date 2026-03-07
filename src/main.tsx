0e13804f67647b1e08f32d168af87039ebbd1c88
Romain <clawadmin@vps-b2edf054.vps.ovh.net>
2026-03-07 18:33:40 +0000

    feat: Sentry error monitoring intégré

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+import * as Sentry from '@sentry/react'
+Sentry.init({
+  dsn: 'https://25d93bc4b45cf5e78f96770da68e0498@o4511004596633600.ingest.de.sentry.io/4511004637659216',
+  environment: import.meta.env.MODE,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.1,
+  replaysOnErrorSampleRate: 0,
+})
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
